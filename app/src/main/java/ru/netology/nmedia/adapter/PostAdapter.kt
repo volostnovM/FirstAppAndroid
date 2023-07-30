27f0fa9b499444436423718fe7ab1e1d056d099e
volostnovM <volostnov@tele2med.ru>
2023-07-30 21:16:30 +0300

    Выполнение дз RecyclerView_2

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -0,0 +1,64 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+typealias OnLikeListener = (post: Post) -> Unit
+
+class PostsAdapter(
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnLikeListener
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener, onShareListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = getItem(position)
+        holder.bind(post)
+    }
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnLikeListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            shareText.text = post.shared.toString()
+            likeText.text = post.likes.toString()
+            likeButton.setImageResource(
+                if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+            )
+            likeButton.setOnClickListener{
+                onLikeListener(post)
+            }
+
+            shareButton.setOnClickListener{
+                onShareListener(post)
+            }
+        }
+    }
+}
+
+
+class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+}
