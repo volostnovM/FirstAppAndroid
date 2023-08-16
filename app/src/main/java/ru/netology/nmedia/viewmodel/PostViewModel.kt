d74a54baddb6c99b41fe8ed44250e01bebaa7060
volostnovM <volostnov@tele2med.ru>
2023-08-16 21:27:31 +0300

    Выполнение дз SharedPreferences

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,10 +1,13 @@
 package ru.netology.nmedia.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPreferences
 
 private val empty = Post(
     id = 0,
@@ -16,8 +19,8 @@ private val empty = Post(
     shared = 0
 )
 
-class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositorySharedPreferences(application)
     val data = repository.get()
 
     val edited = MutableLiveData(empty)
